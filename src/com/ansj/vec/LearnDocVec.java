36819b0068ad60a527f310aa041194db7c208e09
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-11-26 13:32:49 +0700

    add function to handle unseen instances

--- src/com/ansj/vec/LearnDocVec.java
+++ src/com/ansj/vec/LearnDocVec.java
@@ -87,6 +87,7 @@ public class LearnDocVec {
 		createExpTable();
 
 	}
+        long nextRandom = 5;
 
 	/**
 	 * trainModel
@@ -98,7 +99,7 @@ public class LearnDocVec {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(
 				new FileInputStream(file)))) {
 			String temp = null;
-			long nextRandom = 5;
+			nextRandom = 5;
 			int wordCount = 0;
 			int lastWordCount = 0;
 			int wordCountActual = 0;
@@ -166,6 +167,51 @@ public class LearnDocVec {
 			System.out.println("sucess train over!");
 		}
 	}
+        
+        public float[] getUnseenDocVector(String temp){
+            float[] vector = new float[layerSize];
+
+            Random random = new Random();
+
+            for (int i = 0; i < vector.length; i++)
+            vector[i] = (float) ((random.nextDouble() - 0.5) / layerSize);
+            
+
+            String[] strs = temp.split(" ");
+            List<WordNeuron> sentence = new ArrayList<WordNeuron>();
+            
+            for (int i = 0; i < strs.length; i++) {
+                    Neuron entry = wordMap.get(strs[i]);
+                    if (entry == null) {
+                            continue;
+                    }
+                    // The subsampling randomly discards frequent words while
+                    // keeping the ranking same
+                    if (sample > 0) {
+                            double ran = (Math.sqrt(entry.freq
+                                            / (sample * trainWordsCount)) + 1)
+                                            * (sample * trainWordsCount) / entry.freq;
+                            nextRandom = nextRandom * 25214903917L + 11;
+                            if (ran < (nextRandom & 0xFFFF) / (double) 65536) {
+                                    continue;
+                            }
+                    }
+                    sentence.add((WordNeuron) entry);
+            }
+
+            for (int index = 0; index < sentence.size(); index++) {
+                    nextRandom = nextRandom * 25214903917L + 11;
+                    if (isCbow) {
+                            cbowGram(index, vector, sentence, (int) nextRandom
+                                            % window);
+                    } else {
+                            skipGram(index, vector, sentence, (int) nextRandom
+                                            % window);
+                    }
+            }
+
+            return vector;
+        }
 
 	/**
 	 * skip gram 模型训练
@@ -233,6 +279,72 @@ public class LearnDocVec {
 		}
 
 	}
+        
+        
+	/**
+	 * skip gram 模型训练
+	 * 
+	 * @param sentence
+	 * @param neu1
+	 */
+	private void skipGram(int index, float [] doc_vec, List<WordNeuron> sentence,
+			int b) {
+		// TODO Auto-generated method stub
+		WordNeuron word = sentence.get(index);
+		int a, c = 0;
+		for (a = b; a < window * 2 + 1 - b; a++) {
+			if (a == window) {
+				continue;
+			}
+			c = index - window + a;
+			if (c < 0 || c >= sentence.size()) {
+				continue;
+			}
+
+			double[] neu1e = new double[layerSize];// 误差项
+			// HIERARCHICAL SOFTMAX
+			List<Neuron> neurons = word.neurons;
+			// WordNeuron we = sentence.get(c);
+			// 不是中间词向量，而是文本向量
+
+			for (int i = 0; i < neurons.size(); i++) {
+				HiddenNeuron out = (HiddenNeuron) neurons.get(i);
+				double f = 0;
+				// Propagate hidden -> output
+				for (int j = 0; j < layerSize; j++) {
+					// f += we.syn0[j] * out.syn1[j];
+					f += doc_vec[j] * out.syn1[j];
+				}
+				if (f <= -MAX_EXP || f >= MAX_EXP) {
+					continue;
+				} else {
+					f = (f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2);
+					f = expTable[(int) f];
+				}
+				// 'g' is the gradient multiplied by the learning rate
+				double g = (1 - word.codeArr[i] - f) * alpha;
+				// Propagate errors output -> hidden
+				for (c = 0; c < layerSize; c++) {
+					neu1e[c] += g * out.syn1[c];
+				}
+				// Learn weights hidden -> output
+				// for (c = 0; c < layerSize; c++) {
+				// out.syn1[c] += g * we.syn0[c];
+				//
+				// }
+				// 不改变预测的中间词的向量
+			}
+
+			// Learn weights input -> hidden
+			for (int j = 0; j < layerSize; j++) {
+				// we.syn0[j] += neu1e[j];
+
+				doc_vec[j] += neu1e[j];
+				// 更新句子（文本）向量，不更新词向量
+			}
+		}
+
+	}
 
 	/**
 	 * 词袋模型
@@ -247,6 +359,95 @@ public class LearnDocVec {
 		int a, c = 0;
 
 		float[] doc_vec = doc_vector.get(sent_no);
+
+		List<Neuron> neurons = word.neurons;
+		double[] neu1e = new double[layerSize];// 误差项
+		double[] neu1 = new double[layerSize];// 误差项
+		WordNeuron last_word;
+
+		for (a = b; a < window * 2 + 1 - b; a++) {
+
+			if (a != window) {
+				c = index - window + a;
+				if (c < 0)
+					continue;
+				if (c >= sentence.size())
+					continue;
+				last_word = sentence.get(c);
+				if (last_word == null)
+					continue;
+				for (c = 0; c < layerSize; c++)
+					neu1[c] += last_word.syn0[c];
+			}
+
+			for (c = 0; c < layerSize; c++)
+				neu1[c] += doc_vec[c];
+			// 将文本的向量也作为输入
+
+		}
+
+		// HIERARCHICAL SOFTMAX
+		for (int d = 0; d < neurons.size(); d++) {
+			HiddenNeuron out = (HiddenNeuron) neurons.get(d);
+			double f = 0;
+			// Propagate hidden -> output
+			for (c = 0; c < layerSize; c++)
+				f += neu1[c] * out.syn1[c];
+			if (f <= -MAX_EXP)
+				continue;
+			else if (f >= MAX_EXP)
+				continue;
+			else
+				f = expTable[(int) ((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))];
+			// 'g' is the gradient multiplied by the learning rate
+			// double g = (1 - word.codeArr[d] - f) * alpha;
+			// double g = f*(1-f)*( word.codeArr[i] - f) * alpha;
+			double g = f * (1 - f) * (word.codeArr[d] - f) * alpha;
+			//
+			for (c = 0; c < layerSize; c++) {
+				neu1e[c] += g * out.syn1[c];
+			}
+			// Learn weights hidden -> output
+			// for (c = 0; c < layerSize; c++) {
+			// out.syn1[c] += g * neu1[c];
+			// }
+			// 不改变预测的中间词的向量
+
+		}
+		for (a = b; a < window * 2 + 1 - b; a++) {
+			if (a != window) {
+				c = index - window + a;
+				if (c < 0)
+					continue;
+				if (c >= sentence.size())
+					continue;
+				last_word = sentence.get(c);
+				if (last_word == null)
+					continue;
+				for (c = 0; c < layerSize; c++) {
+
+					// last_word.syn0[c] += neu1e[c];
+					doc_vec[c] += neu1e[c];
+					// 更新句子（文本）向量，不更新词向量
+
+				}
+
+			}
+
+		}
+	}
+
+	/**
+	 * 词袋模型
+	 * 
+	 * @param index
+	 * @param sentence
+	 * @param b
+	 */
+	private void cbowGram(int index, float [] doc_vec, List<WordNeuron> sentence,
+			int b) {
+		WordNeuron word = sentence.get(index);
+		int a, c = 0;
 
 		List<Neuron> neurons = word.neurons;
 		double[] neu1e = new double[layerSize];// 误差项
--- src/test/Doc2VecTest.java
+++ src/test/Doc2VecTest.java
@@ -9,6 +9,9 @@ import com.ansj.vec.LearnDocVec;
 import com.ansj.vec.Word2VEC;
 import com.ansj.vec.domain.Neuron;
 import com.ansj.vec.util.ReadWriteFile;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class Doc2VecTest {
 
@@ -35,6 +38,7 @@ public class Doc2VecTest {
 		Map<String, Neuron> word2vec_model = learn.getWord2VecModel();
 
 		LearnDocVec learn_doc = new LearnDocVec(word2vec_model);
+                learn_doc.setIsCbow(Boolean.TRUE);
 
 		learn_doc.learnFile(result);
 
@@ -60,6 +64,30 @@ public class Doc2VecTest {
 		ReadWriteFile.writeFile("file//clinical_doc_200_java.vec",
 				sb.toString());
 
+                File testFile = new File("file//clinicalcases.txt");
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(
+				new FileInputStream(testFile)))) {
+                    int sent_no = 0;
+                    String temp = null;
+                    while ((temp = br.readLine()) != null) {
+                        float [] sent_vector = learn_doc.getUnseenDocVector(temp);
+                        double dist = euclideanDistance(doc_vec.get(sent_no),sent_vector);
+                        System.out.println("distance " + sent_no + " : " + dist);
+                        sent_no++;
+                    }
+                }
 	}
+        
+        public static double euclideanDistance(float [] v1, float [] v2){
+            if (v1.length!=v2.length)
+                return Float.MAX_VALUE;
+            int n = v1.length;
+            double sumSquaredError = 0;
+            for (int i=0;i<n;i++){
+                float elementDiff = v1[i]-v2[i];
+                sumSquaredError += elementDiff*elementDiff;
+            }
+            return Math.sqrt(sumSquaredError);
+        }
 
 }
